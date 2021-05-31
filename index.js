3df070c851e201a5f088eb35ec06d9a619d82c21
nflam <nadja.flam@gmx.net>
2021-05-31 10:56:32 +0800

    creating files and documents

---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const Manager = require("./lib/Manager")
+// const Engineer = require("./lib/Engineer")
+// const Intern = require("./lib/Intern")
+// const path = require('path');
+// const htmlHelpers = require('./src/utils/html');
+// const inquirer = require('inquirer');
+// const teamArray = [];
+// const employee = [];
+// let cards;
+
+// initial manager input
+createManager()
+
+function createManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter your name?",
+        },
+        {
+            type: 'input',
+            message: 'What is your ID number?',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Please enter your office phone number?',
+            name: 'officeNumber'
+        }
+    ]).then(answers => {
+        console.log(answers)
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+        teamArray.push(manager);
+        createEmployee()
+    })
+}
